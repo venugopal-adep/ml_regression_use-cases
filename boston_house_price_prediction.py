0aee1cd484549bcb8d0d3e79edd4ff3ef54556e0
venugopal-adep <92349362+venugopal-adep@users.noreply.github.com>
2024-08-04 16:02:18 +0530

    Update boston_house_price_prediction.py

--- boston_house_price_prediction.py
+++ boston_house_price_prediction.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_regression import LinearRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
